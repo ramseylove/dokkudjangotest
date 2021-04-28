e84f319165b7726a74d0621adb5f260bba36fe76
Ryan V <meyeryan@gmail.com>
2021-04-27 21:14:13 -0600

    changed db backend

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
     }
 }
